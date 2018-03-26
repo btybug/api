4729241d267d3187a65fd02252688fe769f53235
terakyan <edo.terakyan@gmail.com>
2018-03-26 17:04:43 +0400

    at

--- src/views/approved.blade.php
+++ src/views/approved.blade.php
@@ -1,4 +1,33 @@
 @extends('btybug::layouts.admin')
 @section('content')
-    Approved
+    <div class="col-md-12">
+        <h2>Approved</h2>
+        <table class="table table-bordered">
+            <thead>
+            <tr>
+                <th>API</th>
+                <th>User</th>
+                <th>Action</th>
+            </tr>
+            </thead>
+            <tbody id="table_engine">
+            @if(isset($fields))
+                @foreach($fields as $count => $field)
+                    <tr>
+                        <td> </td>
+                        <td>{!! Form::select('column[0][type]',$tbtypes,28,['class'=>'form-control']) !!}</td>
+                        <td>
+                            <span class='btn btn-warning'><i class='fa fa-trash' aria-hidden='true'></i></span>
+                        </td>
+                    </tr>
+                @endforeach
+            @else
+                <tr>
+                    <td colspan="3"> No result</td>
+                </tr>
+            @endif
+
+            </tbody>
+        </table>
+    </div>
 @stop
