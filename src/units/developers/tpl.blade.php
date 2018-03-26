9425acdd34fefd42911620ebf142a0ff0c535245
terakyan <edo.terakyan@gmail.com>
2018-03-26 17:01:53 +0400

    ap

--- src/units/developers/tpl.blade.php
+++
@@ -1,8 +0,0 @@
-<div id="app">
-    <passport-clients></passport-clients>
-    <passport-authorized-clients></passport-authorized-clients>
-    <passport-personal-access-tokens></passport-personal-access-tokens>
-</div>
-
-{!! BBscript(public_path('js/app.js')) !!}
-{!! BBstyle(public_path('css/app.css')) !!}
