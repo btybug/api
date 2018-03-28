cb5be5f89d9c95a48af96b907caa45b202558fa7
terakyan <edo.terakyan@gmail.com>
2018-03-28 15:29:04 +0400

    at

--- src/units/developers/tpl.blade.php
+++ src/units/developers/tpl.blade.php
@@ -3,6 +3,16 @@
     <passport-authorized-clients></passport-authorized-clients>
     <passport-personal-access-tokens></passport-personal-access-tokens>
 </div>
+<div class="fb-login-button" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false"></div>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.12&appId=1098783503535453&autoLogAppEvents=1';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
 
 {!! BBscript(public_path('js/app.js')) !!}
 {!! BBstyle(public_path('css/app.css')) !!}
