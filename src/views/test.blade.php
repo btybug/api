bd5a20685401d5a6f2e9f96d9446209ef0336295
Sahak <hakobyan.sahak88@gmail.com>
2018-04-16 22:56:08 +0400

    udate

--- src/views/test.blade.php
+++ src/views/test.blade.php
@@ -6,11 +6,9 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <script src="{{ url("public/js/jquery-3.2.1.min.js") }}" type="text/javascript"></script>
-
 </head>
 <body>
-<script>
+<script type="javascript">
     if (window.opener != null && !window.opener.closed) {
         window.opener.cms.callback('{!! $code !!}');
         window.close();
