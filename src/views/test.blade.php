fa1b5b6e4059ec654f3f1f840c9850d27ed28112
terakyan <edo.terakyan@gmail.com>
2018-04-17 09:42:00 +0400

    test

--- src/views/test.blade.php
+++ src/views/test.blade.php
@@ -1,24 +1,13 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+
 {{--{!! $code !!}--}}
 <script type="javascript">
-    alert(123)
+    // alert(123)
     // if (window.opener != null && !window.opener.closed) {
     //     console.log(1);
     //     window.opener.cms.callback('x');
     // }
-    //     window.close();
+        window.close();
 
 </script>
-</body>
-</html>
 
 
