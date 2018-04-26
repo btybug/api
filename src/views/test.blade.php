0e857e95b21ebb03dfeb361806b5367d89f3ac8f
midoalone <mido_alone2001@yahoo.com>
2018-04-26 23:06:29 +0200

    Fixing close window with callback

--- src/views/test.blade.php
+++ src/views/test.blade.php
@@ -1,5 +1,21 @@
-{{--{!! $code !!}--}}
+<!DOCTYPE html>
+<!--[if IE 8]>
+<html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]>
+<html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en" class="no-js">
+<!--<![endif]-->
+<head>
+    <meta charset="utf-8"/>
+    <title>Avatarbug</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1" name="viewport"/>
+    <meta content="" name="description"/>
+    <meta content="" name="author"/>
+</head>
 
+<body>
 <script type="javascript">
 
     var finish = function (){
@@ -13,3 +29,5 @@
 </script>
 
 <button onclick="finish()">Close Window</button>
+</body>
+</html>
