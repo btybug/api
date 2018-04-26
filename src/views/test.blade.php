610939728f6a1bcf64020d0cd7d617af0cd98c6c
Sahak <hakobyan.sahak88@gmail.com>
2018-04-26 23:51:39 +0400

    up

--- src/views/test.blade.php
+++ src/views/test.blade.php
@@ -1,11 +1,11 @@
 
 {{--{!! $code !!}--}}
 <script type="javascript">
-    // alert(123)
-    // if (window.opener != null && !window.opener.closed) {
-    //     console.log(1);
-    //     window.opener.cms.callback('x');
-    // }
+    alert(123)
+    if (window.opener != null && !window.opener.closed) {
+        console.log(1);
+        window.opener.cms.callback('x');
+    }
         window.close();
 
 </script>
